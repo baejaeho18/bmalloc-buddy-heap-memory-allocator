6a98e26e007e3994d07b0f90efd1242db76d5fd9
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-05-10 12:10:36 +0900

    Add files via upload

--- bmalloc.c
+++ bmalloc.c
@@ -4,28 +4,36 @@
 
 bm_option bm_mode = BestFit ;
 bm_header bm_list_head = {0, 0, 0x0 } ;	// next points the first header of pages
+bm_header_ptr pre_siblings = 0x0 ;
+bm_header_ptr post_siblings = 0x0 ;
 
+// if h is pre-header of current header, could the code be simple and better?
+// no, it's not.. It doesn't work always like that
 void * sibling (void * h)
 {
 	// TODO : returns the header address of the suspected sibling block of h
 	int size = (bm_header *)h->size ;
 
 	bm_header_ptr itr ;
-	bm_header_ptr pre ;
-	int piled_size = 0 ;
+	bm_header_ptr pre = bm_list_head ;
+	int piled_size = 1 ;	// why?
 
 	for (itr = bm_list_head.next ; itr != 0x0 || itr != (char *)h ; itr = itr->next) {
-		piled_size += itr->size ;
+		piled_size += (1 << itr->size) ;
+		pre_siblings = pre ;
 		pre = itr ;
 	}
 	if (itr == 0x0) {
 		perror("wrong header pointer\n") ;
 	}
-	else if ((piled_size % size) % 2 == 1) { // right side
+	else if ((piled_size % size) % 2 == 1) { // left side
+		pre_siblings = pre ;
+		post_siblings = (char *)h->next->next ;
 		return (char *)h->next ; 
 	}
-	else {	// left side
-		return pre->next ;
+	else {	// right side
+		post_siblings = (char *)h->next ;
+		return pre ;
 	}
 }
 
@@ -111,32 +119,31 @@ void * bmalloc (size_t s)
 void bfree (void * p) 
 {
 	// TODO : free the allocated buffer srarting at pointer p
-	// before header -> next header & header.used->1
-	(char *) p->used = 0 ;
-	
+	// header.used->1
+	(char *)p->used = 0 ;
+
 	// merge with sibiling
+	// before header -> next header
+	// if ((char *)p->size == (char *)sibling(p)->size) {
+	// 	pre_siblings->next == post_siblings ;
+	// }
+	bm_header_ptr itr ;
+	for (itr = p ; itr->size != (char *)sibling(itr)->size ; itr = pre_siblings->next) {
+		pre_siblings->next == post_siblings ;
+	}
 	// if 4096, unmap()
-	if (munmap(p, 4096) == -1) {
-		perror("munmap error\n") ;
-		exit(1) ;
-	};
+	if (itr->size == 4096) {
+		if (munmap(p, 4096) == -1) {
+			perror("munmap error\n") ;
+			exit(1) ;
+		};
+	}
 }
 
 void * brealloc (void * p, size_t s) 
 {
-	// TODO : resize the allocated memory buffer into s bytes
-	if ((char*)p->size < fitting(s)) {
-		// maintain p and slice the size? or just free and alloc?
-		bfree(p) ;
-		return bmalloc(s) ;
-	}
-	else if ((char*)p == fitting(s)) {
-		return p ;
-	}
-	else {	// p.size > s
-		bfree(p) ;
-		return bmalloc(s) ;
-	}
+	bfree(p) ;
+	return bmalloc(s) ;
 }
 
 void bmconfig (bm_option opt) 
@@ -145,12 +152,15 @@ void bmconfig (bm_option opt)
 	bm_mode = opt;
 }
 
-
 void 
 bmprint () 
 {
 	bm_header_ptr itr ;
 	int i ;
+	int total_size ;
+	int total_used_size ;
+	int total_available_size ;
+	int total_internal_fragment ;
 
 	printf("==================== bm_list ====================\n") ;
 	for (itr = bm_list_head.next, i = 0 ; itr != 0x0 ; itr = itr->next, i++) {
@@ -161,10 +171,17 @@ bmprint ()
 		for (j = 0 ; j < (itr->size >= 8 ? 8 : itr->size) ; j++) 
 			printf("%02x ", s[j]) ;
 		printf("\n") ;
+
+		//TODO: print out the stat's.
+		// total size, total used size, total available size, total internal fragment
+		// each block's used, size, payload size
+		total_used_size += (int) itr->size ;
 	}
 	printf("=================================================\n") ;
 
-	//TODO: print out the stat's.
-	// total size, total used size, total available size, total internal fragment
-	// each block's used, size, payload size
+	printf("Total size = %d\n", total_size) ;
+	printf("Total used size = %d\n", total_used_size) ;
+	printf("Total available size = %d\n", total_size - total_used_size) ;
+	printf("Total internal fragment = %d\n", total_internal_fragment) ;
+
 }
